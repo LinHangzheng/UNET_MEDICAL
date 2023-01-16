7ee4e6beb3638d9ae6ea356c1f5e2ce823085ee2
LinHangzheng <hangzheng.17@intl.zju.edu.cn>
2023-01-16 14:16:29 -0600

    update

--- input/dataset.py
+++ input/dataset.py
@@ -48,15 +48,15 @@ class IRDataset(Dataset):
                             )
         del self.IR, self.label
         self.data_augmentation()
-        # for i in range(self.IR_patches.shape[0]):
-        #     img = Image.fromarray(np.array(self.label_patches[i]/6*255,dtype=np.uint8))
+        for i in range(self.IR_patches.shape[0]):
+            img = Image.fromarray(np.array(self.label_patches[i]/6*255,dtype=np.uint8))
             
-        #     img.save(f"{self.mode}_label_{i}.jpg")
-        #     IR = self.IR_patches[i][4] + (0.0001**0.5)*torch.randn(self.IR_patches[i][4].shape)
-        #     img = Image.fromarray(np.array((IR-torch.min(IR))/torch.max(IR)*255,dtype=np.uint8))
-        #     img.save(f"{self.mode}_IR_{i}.jpg")
-        #     img = Image.fromarray(np.array((self.IR_patches[i][4]-torch.min(self.IR_patches[i][4]))/torch.max(self.IR_patches[i][4])*255,dtype=np.uint8))
-        #     img.save(f"{self.mode}_IR_{i}_ori.jpg")
+            img.save(f"{self.mode}_label_{i}.jpg")
+            IR = self.IR_patches[i][4] + (0.0001**0.5)*torch.randn(self.IR_patches[i][4].shape)
+            img = Image.fromarray(np.array((IR-torch.min(IR))/torch.max(IR)*255,dtype=np.uint8))
+            img.save(f"{self.mode}_IR_{i}.jpg")
+            img = Image.fromarray(np.array((self.IR_patches[i][4]-torch.min(self.IR_patches[i][4]))/torch.max(self.IR_patches[i][4])*255,dtype=np.uint8))
+            img.save(f"{self.mode}_IR_{i}_ori.jpg")
             
     def normolize(self, IR):
         negative_idx = np.where(IR<0)
