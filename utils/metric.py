30198cdb644eb63bc74a4dafdcc066528e2de078
LinHangzheng <hangzheng.17@gmail.com>
2023-03-12 16:48:25 -0500

    update plot resolution

--- utils/metric.py
+++ utils/metric.py
@@ -69,6 +69,8 @@ def plot_roc(pre, labels, num_classes, save_path, thresholds=None):
     metric = MulticlassROC(num_classes=num_classes, thresholds=thresholds)
     fpr, tpr, thresholds = metric(pre, y)
     plt.figure()
+    plt.rcParams['figure.dpi'] = 300
+    plt.rcParams['savefig.dpi'] = 300
     print("start plotting")
     for i in range(num_classes):
         plt.plot(fpr[i].cpu(),tpr[i].cpu(),color=slicedCM[i],label=f"ROC_{i+1}")
