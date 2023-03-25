e256fa339051099888caab775278e0ec7435821c
LinHangzheng <hangzheng.17@gmail.com>
2023-03-24 21:04:48 -0500

    update

--- data/data_prepare.py
+++ data/data_prepare.py
@@ -8,6 +8,20 @@ from glob import glob
 from PIL import Image as im
 from tqdm import tqdm
 
+RGB_PALLET = np.array([
+    [35, 101, 173],
+    [48, 177, 85],
+    [76, 134, 178],
+    [192, 100, 68],
+    [132, 46, 30],
+    [234, 36, 46],
+    [78, 44, 134],
+    [158, 58, 138],
+    [217, 194, 216],
+    [144, 126, 153],
+    [255, 255, 255]
+])
+
 def normolize(IR, max_val):
     negative_pos = np.where(IR<0)
     IR[negative_pos] = 0
@@ -15,10 +29,32 @@ def normolize(IR, max_val):
     return IR
 
 
-        
-def save_data(IR_files, folder, max_IR, plot=True):
+
+def save_val_patches(IR, label, img_size, folder, idx, name, plot):
+    h_start = (IR.shape[1]-img_size*4)//2
+    w_start = (IR.shape[2]-img_size*4)//2
+    h_idx  = 0
+    while h_idx < 4:
+        w_idx = 0
+        while w_idx < 4:
+            h = h_start + h_idx*img_size
+            w = w_start + w_idx*img_size
+            IR_patch = IR[:,h:h+img_size,w:w+img_size]
+            label_patch = label[h:h+img_size,w:w+img_size]
+            
+            data_save_plot(re.sub('val_large','val',folder),
+                    IR_patch, 
+                    label_patch, 
+                    f'IR_{idx}_{name}_{h_idx}_{w_idx}',
+                    f'label_{idx}_{name}_{h_idx}_{w_idx}',
+                    plot)
+            
+            w_idx += 1
+        h_idx += 1
+            
+def save_data(IR_files, folder, max_IR, plot=True, train=True):
     for i, IR_file in enumerate(tqdm(IR_files)):
-        idx = IR_file.split('/')[-1].split('.')[0]
+        name = IR_file.split('/')[-1].split('.')[0]
         data = scipy.io.loadmat(IR_file)
         IR = data['IRsub']
         label = data['6S_weakLabel']
@@ -27,9 +63,12 @@ def save_data(IR_files, folder, max_IR, plot=True):
         data_save_plot(folder,
                        IR, 
                        label, 
-                       f'IR_{i}_{idx}',
-                       f'label_{i}_{idx}',
+                       f'IR_{i}_{name}',
+                       f'label_{i}_{name}',
                        plot)
+        
+        if not train:
+            save_val_patches(IR, label, 224, folder, i, name, plot)
                
 def data_save_plot(save_folder,
                    IR_patch, 
@@ -46,11 +85,12 @@ def data_save_plot(save_folder,
         return
     
     IR_patch = (IR_patch[0,:,:]/np.max(IR_patch[0,:,:])*255).astype(np.uint8)
-    Label_patch = (Label_patch/6*255).astype(np.uint8)
-    
+    labels_RGB = np.zeros([Label_patch.shape[0],Label_patch.shape[1],3]).astype(np.uint8)
+    for k in range(7):
+        labels_RGB[np.where(Label_patch==k)] = RGB_PALLET[k]
     img = im.fromarray(IR_patch)
     img.save(os.path.join(save_folder, 'IR', f'{IR_save_name}.jpeg'))
-    img = im.fromarray(Label_patch)
+    img = im.fromarray(labels_RGB)
     img.save(os.path.join(save_folder, 'label', f'{Label_save_name}.jpeg'))
        
         
@@ -77,21 +117,25 @@ def prepare_data(large_IR_files,
     patches_train = large_IR_files[:int(len(large_IR_files)*train_test_split)]
     patches_test = large_IR_files[int(len(large_IR_files)*train_test_split):]
     
-    save_data(patches_train, train_folder, max_IR, plot)
-    save_data(patches_test, test_folder, max_IR, plot)
+    save_data(patches_train, train_folder, max_IR, plot, True)
+    save_data(patches_test, test_folder, max_IR, plot, False)
 
 if __name__ == "__main__":
-    train_folder = '/raid/projects/hangzheng/data/train'
-    test_folder = '/raid/projects/hangzheng/data/val'
+    root = '/home/hangzheng/tissue_segmentation/data'
+    train_folder = os.path.join(root, 'train')
+    test_large_folder = os.path.join(root, 'val_large')
+    test_folder = os.path.join(root, 'val')
+    
     plot = True
-    IR_files = sorted(glob("/raid/projects/hangzheng/BR1003_Cores/Data/*"))
+    IR_files = sorted(glob("/home/hangzheng/tissue_segmentation/data/BR1003_Cores/Data/*"))
     
     train_test_split = 0.8
     create_folder(train_folder)
+    create_folder(test_large_folder)
     create_folder(test_folder)
 
     prepare_data(IR_files,
                        train_test_split,
                        train_folder,
-                       test_folder,
+                       test_large_folder,
                        plot)
